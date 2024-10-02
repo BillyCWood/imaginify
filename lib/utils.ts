628d68eee288b579a76e025ba17f271e47b2eeb2
Billy Wood <billyc.wood4@gmail.com>
2024-10-01 23:49:01 -0600

    disable eslint rule prefer-spread

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-prototype-builtins */
+/* eslint-disable prefer-spread */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { type ClassValue, clsx } from "clsx";
 import qs from "qs";
